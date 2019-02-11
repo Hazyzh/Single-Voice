fb4aba6c19964701d609c9ef1f11b593ebff3e9e
Harry.Hou <harry.hou@ringcentral.com>
2019-02-11 22:00:24 +0800

    :bug: (index.js) fix toggle setting data issues'

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ const switchBtn = document.getElementById('switch')
 const labelText = document.getElementById('labelText')
 
 switchBtn.onclick = e => {
-  if (!this.toggle) {
-    this.toggle = true
+  const checked = e.target.checked
+  if (checked) {
     labelText.innerText = '/on'
     chrome.runtime.sendMessage({ event: 'start' })
   } else {
-    this.toggle = false
     labelText.innerText = '/off'
     chrome.runtime.sendMessage({ event: 'end' })
   }
+  chrome.storage.sync.set({voiceToggle: checked}, function() {
+    console.log('@:Current setting is ' + checked)
+  })
 }
+chrome.storage.sync.get('voiceToggle', function(data) {
+  if (data.voiceToggle) {
+    switchBtn.checked = data.voiceToggle
+  }
+});
 
